79e22aef9f5a5e4c6762ffd922a23d42b2ad7421
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2021-09-23 14:55:42 +0200

    Remove use of env variables

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,7 +15,7 @@ const PROXY_REGISTRY = {
 const deploy = {
   token: async (ctx) => {
     const Token = await ethers.getContractFactory("GSAT");
-    ctx.token = await Token.deploy(process.env.NETWORK === "Rinkeby" ? PROXY_REGISTRY.rinkeby : PROXY_REGISTRY.mainnet);
+    ctx.token = await Token.deploy(PROXY_REGISTRY.rinkeby);
     await ctx.token.deployed();
     console.log(`Token deployed at ${chalk.cyan(ctx.token.address)} and arkhe batch minted`);
     await (await ctx.token.mint(Batch.Drop)).wait();
